c31c58c77a550bc6bc7621f0a179acec95b94816
DominikTenerife <dpiskorski1@gmail.com>
2024-10-11 12:35:41 +0100

    adding jQuery scripts to index.html and adding basing resizing logic

--- pages/Example1.jsx
+++ pages/Example1.jsx
@@ -1,11 +1,41 @@
 
+import '../ResizableColumns.css';
+import  { useEffect } from 'react';
+
 export default function Example1() {
 
-    return (
-    <div className="container-main">
-      <div className="container1">Container 1</div>
-      <div className="container2">Container 2</div>
-      <div className="container3">Container 3</div>
-    </div>
-    )
+    useEffect(() => {
+        // Make sure jQuery UI is loaded after jQuery
+        if (typeof window.$.fn.resizable === 'function') {
+          window.$('.container1').resizable({
+            handles: 'e',
+            resize: function (event, ui) {
+              const container1Width = ui.size.width;
+              const container2Width = window.$('.container2').width();
+              const remainingWidth = window.$('.container-main').width() - container1Width - container2Width;
+              window.$('.container3').width(remainingWidth);
+            },
+          });
+    
+          window.$('.container2').resizable({
+            handles: 'e',
+            resize: function (event, ui) {
+              const container1Width = window.$('.container1').width();
+              const container2Width = ui.size.width;
+              const remainingWidth = window.$('.container-main').width() - container1Width - container2Width;
+              window.$('.container3').width(remainingWidth);
+            },
+          });
+        } else {
+          console.error('jQuery UI resizable is not available.');
+        }
+      }, []);
+    
+      return (
+        <div className="container-main">
+          <div className="container1">Container 1</div>
+          <div className="container2">Container 2</div>
+          <div className="container3">Container 3</div>
+        </div>
+      );
 }
